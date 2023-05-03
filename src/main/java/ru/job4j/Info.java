016291f206b8ef9beeed5494f7fe6ecaccef3092
Kudo040500 <yuzbashevakop@yandex.ru>
2023-05-03 23:20:30 +0300

    1. Определите тип данных для переменных. [#172634]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -3,6 +3,10 @@ package ru.job4j;
 public class Info {
     public static void main(String[] args) {
         System.out.println("03.05.2023");
+        int age = 16;
+        System.out.println(age);
+        char symbol = 'A';
+        System.out.println(symbol);
 
     }
 }
